71820b8960e26a21476f3ec8eeab7f8100260f9e
Vladys <vladys.z96@gmail.com>
2026-04-27 21:11:01 +0200

    feat: dominio saludlongevidad.org conectado

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://saludlongevidad-web.vercel.app',
+  site: 'https://saludlongevidad.org',
   output: 'static',
   integrations: [
     sitemap({
